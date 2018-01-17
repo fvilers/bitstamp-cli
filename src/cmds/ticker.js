1684f55b2080681b2f4eba5e056ef945524d61c7
Fabian Vilers <fabian.vilers@dev-one.com>
2018-01-17 13:59:47 +0100

    Add bitstamp call to ticker endpoint

--- src/cmds/ticker.js
+++ src/cmds/ticker.js
@@ -1,11 +1,19 @@
+import { promisify } from 'util';
+import Bitstamp from 'bitstamp';
 import { SUPPORTED_CURRENCY_PAIRS } from '../constants';
+import { asyncWrapper } from '../util';
 
-const tickerHandler = (yargs) => {
-  console.log('ticker command is running', yargs);
+const bitstamp = new Bitstamp();
+
+const tickerHandler = async (argv) => {
+  const { currency_pair } = argv;
+  const result = await promisify(bitstamp.ticker)(currency_pair);
+
+  console.log(result);
 };
 
 export const command = 'ticker <currency_pair>';
 export const description = 'Returns data for the specified currency pair.';
 export const builder = yargs => yargs
   .choices('currency_pair', SUPPORTED_CURRENCY_PAIRS);
-export const handler = tickerHandler;
+export const handler = asyncWrapper(tickerHandler);
