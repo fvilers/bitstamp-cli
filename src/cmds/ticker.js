da4f75f8859785f2e457b10a48899b587d3fec5d
Fabian Vilers <fabian.vilers@dev-one.com>
2018-01-17 17:27:08 +0100

    Add hourly option

--- src/cmds/ticker.js
+++ src/cmds/ticker.js
@@ -6,29 +6,36 @@ import { asyncWrapper } from '../util';
 
 const bitstamp = new Bitstamp();
 const tickerHandler = async (argv) => {
-  const { currency_pair } = argv;
-  const data = await promisify(bitstamp.ticker)(currency_pair);
+  const { currency_pair, hourly } = argv;
+  const method = hourly ? bitstamp.ticker_hour : bitstamp.ticker;
+  const data = await promisify(method)(currency_pair);
   const currency = currency_pair
     .slice(0, 3)
     .toLocaleUpperCase();
+  const desc = hourly ? 'Last hour' : 'Last 24 hours';
 
   const rows = [
     ['Date and time', new Date(data.timestamp * 1000).toLocaleString()],
     [`Last ${currency} price`, data.last],
-    ['Last 24 hours price high', data.high],
-    ['Last 24 hours price low', data.low],
-    ['Last 24 hours volume weighted average price', data.vwap],
-    ['Last 24 hours volume', data.volume],
+    [`${desc} price high`, data.high],
+    [`${desc} price low`, data.low],
+    [`${desc} volume weighted average price`, data.vwap],
+    [`${desc} volume`, data.volume],
     ['Highest buy order', data.bid],
     ['Lowest sell order', data.ask],
     ['First price of the day', data.open]
   ];
 
   console.log(table(rows));
+  console.log(argv);
 };
 
 export const command = 'ticker <currency_pair>';
 export const description = 'Returns data for the specified currency pair.';
 export const builder = yargs => yargs
-  .choices('currency_pair', SUPPORTED_CURRENCY_PAIRS);
+  .choices('currency_pair', SUPPORTED_CURRENCY_PAIRS)
+  .option('hourly', {
+    alias: 'h',
+    type: 'boolean'
+  });
 export const handler = asyncWrapper(tickerHandler);
