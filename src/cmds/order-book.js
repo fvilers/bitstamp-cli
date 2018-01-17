44713fe3ce50af1d99f047bcbccb47dcae7319ae
Fabian Vilers <fabian.vilers@dev-one.com>
2018-01-17 20:12:17 +0100

    Implement order book data query

--- src/cmds/order-book.js
+++ src/cmds/order-book.js
@@ -1,11 +1,36 @@
+import { promisify } from 'util';
+import Bitstamp from 'bitstamp';
+import _ from 'lodash';
+import { table } from 'table';
 import { SUPPORTED_CURRENCY_PAIRS } from '../constants';
+import { asyncWrapper } from '../util';
 
-const orderBookHandler = (argv) => {
-  console.log('order book command running', argv);
+const bitstamp = new Bitstamp();
+const orderBookHandler = async (argv) => {
+  const { currency_pair } = argv;
+  const data = await promisify(bitstamp.order_book)(currency_pair);
+  const empty = ['', ''];
+  const formatLine = line => [
+    line[0] && line[1] ? line[0] * line[1] : '', // Value
+    line[1], line[0], // Bid
+    line[2], // Ask
+    line[3], // Amount
+    line[2] && line[3] ? line[2] * line[3] : '' // value
+  ];
+
+  const rows = [
+    ['Value', 'Amount', 'Bid', 'Ask', 'Amount', 'Value'],
+    ..._
+      .zip(data.bids, data.asks)
+      .map(line => [...(line[0] || empty), ...(line[1] || empty)])
+      .map(formatLine)
+  ];
+
+  console.log(table(rows));
 };
 
 export const command = 'orderbook <currency_pair>';
 export const description = 'Returns data like the ticker call, with the calculated values being from within an hour, for the specified currency pair.';
 export const builder = yargs => yargs
   .choices('currency_pair', SUPPORTED_CURRENCY_PAIRS);
-export const handler = orderBookHandler;
+export const handler = asyncWrapper(orderBookHandler);
