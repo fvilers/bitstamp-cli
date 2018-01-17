3511e416c17e12e30123f9a5966bd856cff47033
Fabian Vilers <fabian.vilers@dev-one.com>
2018-01-17 13:55:06 +0100

    Add currency_pair choice

--- src/cmds/ticker.js
+++ src/cmds/ticker.js
@@ -1,7 +1,11 @@
-const tickerHandler = () => {
-  console.log('ticker command is running');
+import { SUPPORTED_CURRENCY_PAIRS } from '../constants';
+
+const tickerHandler = (yargs) => {
+  console.log('ticker command is running', yargs);
 };
 
 export const command = 'ticker <currency_pair>';
 export const description = 'Returns data for the specified currency pair.';
+export const builder = yargs => yargs
+  .choices('currency_pair', SUPPORTED_CURRENCY_PAIRS);
 export const handler = tickerHandler;
