8df1d63a358778f57b33c109081cdefdb9df1203
Fabian Vilers <fabian.vilers@dev-one.com>
2018-01-18 17:55:21 +0100

    Add red color to error message

--- src/util/async-wrapper.js
+++ src/util/async-wrapper.js
@@ -1,5 +1,7 @@
+import chalk from 'chalk';
+
 export const asyncWrapper =
   fn =>
     argv =>
       Promise.resolve(fn(argv))
-        .catch(err => console.error(err.message || err));
+        .catch(err => console.error(chalk.bold.red(err.message || err)));
