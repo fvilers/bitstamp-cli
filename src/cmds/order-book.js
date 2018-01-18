16ed59c43ff65814858a68c3e535699bba668828
Fabian Vilers <fabian.vilers@dev-one.com>
2018-01-18 16:59:23 +0100

    Add the line argument and slice data

--- src/cmds/order-book.js
+++ src/cmds/order-book.js
@@ -8,7 +8,7 @@ import { asyncWrapper, numberToLocaleString } from '../util';
 
 const bitstamp = new Bitstamp();
 const orderBookHandler = async (argv) => {
-  const { currency_pair } = argv;
+  const { currency_pair, line } = argv;
   const data = await promisify(bitstamp.order_book)(currency_pair);
   const empty = ['', ''];
   const formatLine = line => [
@@ -17,13 +17,14 @@ const orderBookHandler = async (argv) => {
     chalk.bold.green(line[0]), // Bid
     chalk.bold.red(line[2]), // Ask
     line[3], // Amount
-    line[2] && line[3] ? numberToLocaleString(line[2] * line[3]) : '' // value
+    line[2] && line[3] ? numberToLocaleString(line[2] * line[3]) : '' // Value
   ];
 
   const rows = [
     ['Value', 'Amount', 'Bid', 'Ask', 'Amount', 'Value'],
     ..._
       .zip(data.bids, data.asks)
+      .slice(0, line || Math.max(data.bids.length, data.asks.length))
       .map(line => [...(line[0] || empty), ...(line[1] || empty)])
       .map(formatLine)
   ];
@@ -34,5 +35,9 @@ const orderBookHandler = async (argv) => {
 export const command = 'orderbook <currency_pair>';
 export const description = 'Returns data like the ticker call, with the calculated values being from within an hour, for the specified currency pair.';
 export const builder = yargs => yargs
-  .choices('currency_pair', SUPPORTED_CURRENCY_PAIRS);
+  .choices('currency_pair', SUPPORTED_CURRENCY_PAIRS)
+  .option('l', {
+    alias: 'line',
+    type: 'number'
+  });
 export const handler = asyncWrapper(orderBookHandler);
